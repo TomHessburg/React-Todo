c5a7bce5c163dfd171433716b0f83e7ca13cd9ae
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-02-14 16:10:20 -0500

    styles added on click, need to create a delete button now

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ToDo = props => {
     return(
-        <div className="todo-item" onClick={() => props.toggleItem(props.task.id)}>
+        <div className={`todo-item ${props.task.completed ? ' task-complete' : ''}`} onClick={() => props.toggleItem(props.task.id)}>
             <p>{props.taskName}</p>
         </div>
     );
